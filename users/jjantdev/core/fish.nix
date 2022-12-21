0c63045bca226bd62d4187bc0747e691eecfb73d
Julian Antonielli <julianantonielli@gmail.com>
2022-12-21 12:06:17 +0000

    Add some fish abbrs

--- users/jjantdev/core/fish.nix
+++ users/jjantdev/core/fish.nix
@@ -6,6 +6,16 @@
         # Disable fish greeting
         set -g fish_greeting
       '';
+
+      shellAbbrs = {
+        gst = "git status";
+
+        bz = "brazil";
+        bw = "brazil workspace";
+        bb = "brazil-build";
+        tx = "toolbox";
+        wp = "cd ~/workplace";
+      };
     };
   };
 }
