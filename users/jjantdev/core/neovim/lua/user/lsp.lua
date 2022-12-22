eee8fa3370cae11117d28da483211cd9147ebe48
Julian Antonielli <julianantonielli@gmail.com>
2022-12-22 12:55:47 +0000

    Add some lsp bindings

--- users/jjantdev/core/neovim/lua/user/lsp.lua
+++ users/jjantdev/core/neovim/lua/user/lsp.lua
@@ -3,3 +3,11 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.nvim_workspace()
 lsp.setup()
+
+lsp.on_attach(function(client, bufnr)
+  print("Attached")
+  local opts = { buffer = bufnr, remap = false }
+
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+end)
