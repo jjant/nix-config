a88586917674e8279a682d1997b82a1ffd0d61dc
Julian Antonielli <julianantonielli@gmail.com>
2023-01-02 22:50:49 +0000

    Import `harpoon` module

--- users/jjantdev/core/neovim/init.lua
+++ users/jjantdev/core/neovim/init.lua
@@ -2,6 +2,7 @@ require('user.options')
 require('user.lsp')
 require('user.treesitter')
 require('user.keymap')
+require('user.harpoon')
 require('user.telescope')
 
 vim.cmd.colorscheme('darkplus')
