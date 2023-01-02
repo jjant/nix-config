cb8947942993d507eca71a4c4a79055fe19fac1b
Julian Antonielli <julianantonielli@gmail.com>
2023-01-02 22:54:31 +0000

    Use `<Leader>d` to delete without yanking

--- users/jjantdev/core/neovim/lua/user/keymap.lua
+++ users/jjantdev/core/neovim/lua/user/keymap.lua
@@ -4,3 +4,7 @@ vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR><Esc>")
 -- Keep selection while indenting
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
+
+-- Delete without yanking
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
