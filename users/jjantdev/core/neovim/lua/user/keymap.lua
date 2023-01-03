3413ab654d62f5864a2e03355dc9256f220b6459
Julian Antonielli <julianantonielli@gmail.com>
2023-01-03 08:47:03 +0000

    Add `<Leader><Leader>` for `<C-^>`

--- users/jjantdev/core/neovim/lua/user/keymap.lua
+++ users/jjantdev/core/neovim/lua/user/keymap.lua
@@ -1,10 +1,12 @@
 -- Hide search results by pressing Esc on normal mode
-vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR><Esc>")
+vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR><Esc>')
 
 -- Keep selection while indenting
-vim.keymap.set("v", ">", ">gv")
-vim.keymap.set("v", "<", "<gv")
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv')
 
 -- Delete without yanking
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set('n', '<Leader>d', '\'_d')
+vim.keymap.set('v', '<Leader>d', '\'_d')
+
+vim.keymap.set('n', '<Leader><Leader>', '<C-^>')
