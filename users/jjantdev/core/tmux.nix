1aa28e31a27b6dd80330dbaf3d3d851de20740c1
Julian Antonielli <julianantonielli@gmail.com>
2023-01-06 18:19:29 +0000

    Add tmux bindings to move between panes

--- users/jjantdev/core/tmux.nix
+++ users/jjantdev/core/tmux.nix
@@ -8,6 +8,12 @@
       bind '"' split-window -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
+
+      # Alt+hjkl to move between panes (macOS)
+      bind -n ˙ select-pane -L
+      bind -n ∆ select-pane -D
+      bind -n ˚ select-pane -U
+      bind -n ¬ select-pane -R
     '';
   };
 }
