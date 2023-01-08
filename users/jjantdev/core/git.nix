b0d57f8de1a387eff1b231f55c87c509599507f3
Julian Antonielli <julianantonielli@gmail.com>
2023-01-08 19:03:04 +0000

    Finish porting `git` config

--- users/jjantdev/core/git.nix
+++ users/jjantdev/core/git.nix
@@ -4,6 +4,16 @@
 
     userName = "Julian Antonielli";
     userEmail = "julianantonielli@gmail.com";
+
+    extraConfig = {
+      color.ui = true;
+      init.defaultBranch = "master";
+      pull.rebase = false;
+      push = {
+        default = "current";
+        autoSetupRemote = true;
+      };
+    };
   };
 
   home.shellAliases = {
