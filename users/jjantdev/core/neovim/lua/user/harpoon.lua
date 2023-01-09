2c5a30212bcaa902efe90e724a0f129c552db09d
Julian Antonielli <julianantonielli@gmail.com>
2023-01-09 15:30:02 +0000

    Improve description of harpoon keymaps

--- users/jjantdev/core/neovim/lua/user/harpoon.lua
+++ users/jjantdev/core/neovim/lua/user/harpoon.lua
@@ -1,10 +1,10 @@
 local harpoon_mark = require('harpoon.mark')
 local harpoon_ui = require('harpoon.ui')
 
-vim.keymap.set("n", "<C-y>", harpoon_mark.add_file)
-vim.keymap.set("n", "<C-e>", harpoon_ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-y>", harpoon_mark.add_file, { desc = "Harpoon: Add file" })
+vim.keymap.set("n", "<C-e>", harpoon_ui.toggle_quick_menu, { desc = "Harpoon: toggle quick menu" })
 
-vim.keymap.set("n", "<C-h>", function() harpoon_ui.nav_file(1) end, { desc = "Go to harpoon file 1" })
-vim.keymap.set("n", "<C-j>", function() harpoon_ui.nav_file(2) end, { desc = "Go to harpoon file 2" })
-vim.keymap.set("n", "<C-k>", function() harpoon_ui.nav_file(3) end, { desc = "Go to harpoon file 3" })
-vim.keymap.set("n", "<C-l>", function() harpoon_ui.nav_file(4) end, { desc = "Go to harpoon file 4" })
+vim.keymap.set("n", "<C-h>", function() harpoon_ui.nav_file(1) end, { desc = "Harpoon: Go to file 1" })
+vim.keymap.set("n", "<C-j>", function() harpoon_ui.nav_file(2) end, { desc = "Harpoon: Go to file 2" })
+vim.keymap.set("n", "<C-k>", function() harpoon_ui.nav_file(3) end, { desc = "Harpoon: Go to file 3" })
+vim.keymap.set("n", "<C-l>", function() harpoon_ui.nav_file(4) end, { desc = "Harpoon: Go to file 4" })
