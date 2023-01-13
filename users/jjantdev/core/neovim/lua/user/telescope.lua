617e8933e209de20ca32f587f49b1e8136fadf08
Julian Antonielli <julianantonielli@gmail.com>
2023-01-13 18:17:19 +0000

    Use `<Ctrl-g>` for Telescope's `live_grep`

--- users/jjantdev/core/neovim/lua/user/telescope.lua
+++ users/jjantdev/core/neovim/lua/user/telescope.lua
@@ -1,3 +1,4 @@
 local telescope_builtin = require('telescope.builtin')
 
-vim.keymap.set("n", "<C-p>", telescope_builtin.git_files)
+vim.keymap.set('n', '<C-p>', telescope_builtin.git_files)
+vim.keymap.set('n', '<C-g>', telescope_builtin.live_grep)
