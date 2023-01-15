17b33b1ff80c9860495c025223dd903064af8247
Julian Antonielli <julianantonielli@gmail.com>
2023-01-15 12:09:40 +0000

    Add `<Leader>fh` for Telescope `help_tags`

--- users/jjantdev/core/neovim/lua/user/telescope.lua
+++ users/jjantdev/core/neovim/lua/user/telescope.lua
@@ -2,3 +2,4 @@ local telescope_builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', telescope_builtin.git_files)
 vim.keymap.set('n', '<C-g>', telescope_builtin.live_grep)
+vim.keymap.set('n', '<Leader>fh', telescope_builtin.help_tags)
