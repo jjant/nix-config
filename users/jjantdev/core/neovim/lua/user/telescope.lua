abe8d04c7e261aea74accf1bdd299b06abfd50c6
Julian Antonielli <julianantonielli@gmail.com>
2023-01-16 14:30:30 +0000

    Use `<Leader>ff` for Telescope `find_files`

--- users/jjantdev/core/neovim/lua/user/telescope.lua
+++ users/jjantdev/core/neovim/lua/user/telescope.lua
@@ -2,4 +2,7 @@ local telescope_builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', telescope_builtin.git_files)
 vim.keymap.set('n', '<C-g>', telescope_builtin.live_grep)
+
 vim.keymap.set('n', '<Leader>fh', telescope_builtin.help_tags)
+
+vim.keymap.set('n', '<Leader>ff', telescope_builtin.find_files)
