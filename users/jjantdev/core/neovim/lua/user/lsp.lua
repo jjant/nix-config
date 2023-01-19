e135d4490df81ab8a4d10d28ff27990292a6a468
Julian Antonielli <julianantonielli@gmail.com>
2023-01-19 18:40:49 +0000

    Add `]d`, `[d` bindings to move to diagnostics

--- users/jjantdev/core/neovim/lua/user/lsp.lua
+++ users/jjantdev/core/neovim/lua/user/lsp.lua
@@ -1,13 +1,25 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('system-lsp')
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+})
+
 lsp.setup_servers({ 'rust_analyzer', 'rnix', 'sumneko_lua' })
 lsp.nvim_workspace()
 lsp.setup()
 
 local format_augroup = vim.api.nvim_create_augroup('LspFormat', { clear = true })
 
-function create_format_autocmd()
+local create_format_autocmd = function()
   vim.api.nvim_clear_autocmds({ group = format_augroup })
   vim.api.nvim_create_autocmd('BufWritePre', { callback = function()
     vim.lsp.buf.format({ async = false })
@@ -19,6 +31,9 @@ lsp.on_attach(function(_, bufnr)
 
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
 
   create_format_autocmd()
 end)
