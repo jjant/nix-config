dcc5f52327977225cc8a5d42527fff147046276d
Julian Antonielli <julianantonielli@gmail.com>
2023-01-20 23:10:27 +0000

    Highlight yanked in neovim

--- users/jjantdev/core/neovim/init.lua
+++ users/jjantdev/core/neovim/init.lua
@@ -6,7 +6,25 @@ require('user.keymap')
 require('user.harpoon')
 require('user.telescope')
 
-vim.cmd.colorscheme('darkplus')
-
 require('marks').setup()
 require('gitsigns').setup()
+
+vim.cmd.colorscheme('darkplus')
+
+local highlight_yanked = function(args)
+  local timeout = args.timeout
+  local yank_group = vim.api.nvim_create_augroup('YankAugroup', { clear = true })
+
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+      vim.highlight.on_yank({
+        higroup = 'IncSearch',
+        timeout = timeout,
+      })
+    end,
+  })
+end
+
+highlight_yanked({ timeout = 125 })
