6d51f50747f332c40a1752b490f98aa19f80fb51
Julian Antonielli <julianantonielli@gmail.com>
2023-11-26 22:28:17 +0000

    Don't use skhd command to focus window on space switch
    This was really buggy and apparently this was already working by
    default, see https://github.com/koekeishiya/yabai/issues/1053.

--- hosts/discovery/skhd.nix
+++ hosts/discovery/skhd.nix
@@ -12,15 +12,15 @@
 
     meh - f : yabai -m window --toggle zoom-fullscreen
 
-    ctrl - 1 : yabai -m space --focus 1 && yabai -m window --focus first
-    ctrl - 2 : yabai -m space --focus 2 && yabai -m window --focus first
-    ctrl - 3 : yabai -m space --focus 3 && yabai -m window --focus first
-    ctrl - 4 : yabai -m space --focus 4 && yabai -m window --focus first
-    ctrl - 5 : yabai -m space --focus 5 && yabai -m window --focus first
-    ctrl - 6 : yabai -m space --focus 6 && yabai -m window --focus first
-    ctrl - 7 : yabai -m space --focus 7 && yabai -m window --focus first
-    ctrl - 8 : yabai -m space --focus 8 && yabai -m window --focus first
-    ctrl - 9 : yabai -m space --focus 9 && yabai -m window --focus first
+    ctrl - 1 : yabai -m space --focus 1
+    ctrl - 2 : yabai -m space --focus 2
+    ctrl - 3 : yabai -m space --focus 3
+    ctrl - 4 : yabai -m space --focus 4
+    ctrl - 5 : yabai -m space --focus 5
+    ctrl - 6 : yabai -m space --focus 6
+    ctrl - 7 : yabai -m space --focus 7
+    ctrl - 8 : yabai -m space --focus 8
+    ctrl - 9 : yabai -m space --focus 9
 
     ctrl - left : yabai -m space --focus prev && yabai -m window --focus first
     ctrl - right : yabai -m space --focus next && yabai -m window --focus first
