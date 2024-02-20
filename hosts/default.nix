e4ad1932c91a8f89a5e2fccf9a08d9210f38cda8
Julian Antonielli <julianantonielli@gmail.com>
2024-02-20 14:55:14 +0000

    WIP building different types of hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,12 +1,28 @@
+{ nix-darwin, lib, ... }:
+let
+  hosts =
+    lib.mapAttrs (hostName: host: host // { name = hostName; }) {
+
+      # Work laptop
+      endeavour = {
+        type = "nix-darwin";
+        platform = "aarch64-darwin";
+      };
+      # Cloud desktop
+      discovery = {
+        type = "home-manager";
+        platform = "aarch64-linux";
+      };
+    };
+
+  buildDarwinHost = { host, configuration }: {
+    darwinConfigurations.${host.name} = nix-darwin.lib.darwinSystem {
+      # TODO: Fill in modules
+      modules = [ ];
+    };
+  };
+in
 {
-  # Work laptop
-  endeavour = {
-    type = "nix-darwin";
-    platform = "aarch64-darwin";
-  };
-  # Cloud desktop
-  discovery = {
-    type = "home-manager";
-    platform = "aarch64-linux";
-  };
+  inherit hosts;
+
 }
