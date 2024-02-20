8bb89d515ebcc37b5bf15586be4931d2a8c3e307
Julian Antonielli <julianantonielli@gmail.com>
2024-02-20 14:54:35 +0000

    Add `<Cmd-{number}>` alacritty bindings

--- users/jjantdev/alacritty.nix
+++ users/jjantdev/alacritty.nix
@@ -1,3 +1,17 @@
+let
+  # Make `<CMD-{number}>` send `<CTRL-B>{number}`
+  tmuxBindings = [
+    { key = "1"; mods = "Command"; chars = "\\u00021"; }
+    { key = "2"; mods = "Command"; chars = "\\u00022"; }
+    { key = "3"; mods = "Command"; chars = "\\u00023"; }
+    { key = "4"; mods = "Command"; chars = "\\u00024"; }
+    { key = "5"; mods = "Command"; chars = "\\u00025"; }
+    { key = "6"; mods = "Command"; chars = "\\u00026"; }
+    { key = "7"; mods = "Command"; chars = "\\u00027"; }
+    { key = "8"; mods = "Command"; chars = "\\u00028"; }
+    { key = "9"; mods = "Command"; chars = "\\u00029"; }
+  ];
+in
 {
   programs.alacritty = {
     enable = true;
@@ -17,6 +31,9 @@
           style = "Semibold";
         };
         size = 15;
+      };
+      keyboard = {
+        bindings = tmuxBindings;
       };
     };
   };
