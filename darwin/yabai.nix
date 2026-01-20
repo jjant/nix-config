ca18fd283cacbd431d6068115dad603719df00ce
Julian Antonielli <julianantonielli@gmail.com>
2026-01-20 12:50:16 +0000

    Temporarily disable yabai since it doesn't seem to work well in Mac26

--- darwin/yabai.nix
+++ darwin/yabai.nix
@@ -1,17 +1,18 @@
 {lib, ...}: {
-  services.yabai.enable = true;
-  services.yabai.enableScriptingAddition = true;
+  # TODO: Not working well in Mac26.
+  services.yabai.enable = false;
+  services.yabai.enableScriptingAddition = false;
   # services.yabai.extraConfig = builtins.trace (lib.readFile ./yabairc) (lib.readFile ./yabairc);
-  services.yabai.config = {
-    layout = "bsp";
-    top_padding         = 5;
-    bottom_padding      = 5;
-    left_padding        = 5;
-    right_padding       = 5;
-    window_gap          = 5;
-    window_opacity      = "off";
-  };
-  services.yabai.extraConfig = ''
-    yabai -m rule --add app='System Settings' manage=off
-  '';
+  # services.yabai.config = {
+  #   layout = "bsp";
+  #   top_padding         = 5;
+  #   bottom_padding      = 5;
+  #   left_padding        = 5;
+  #   right_padding       = 5;
+  #   window_gap          = 5;
+  #   window_opacity      = "off";
+  # };
+  # services.yabai.extraConfig = ''
+  #   yabai -m rule --add app='System Settings' manage=off
+  # '';
 }
