42858c81486e6b38d37777744b9e4c6586d5ffb6
Julian Antonielli <julianantonielli@gmail.com>
2026-06-10 13:54:38 +0100

    fix: alacritty CMD+number keybindings for TOML format

--- users/jjantdev/alacritty.nix
+++ users/jjantdev/alacritty.nix
@@ -1,15 +1,17 @@
 let
   # Make `<CMD-{number}>` send `<CTRL-B>{number}`
+  # We embed the raw STX (0x02) character so TOML serialization preserves it
+  prefix = builtins.fromJSON ''"\u0002"'';
   tmuxBindings = [
-    { key = "1"; mods = "Command"; chars = "\\u00021"; }
-    { key = "2"; mods = "Command"; chars = "\\u00022"; }
-    { key = "3"; mods = "Command"; chars = "\\u00023"; }
-    { key = "4"; mods = "Command"; chars = "\\u00024"; }
-    { key = "5"; mods = "Command"; chars = "\\u00025"; }
-    { key = "6"; mods = "Command"; chars = "\\u00026"; }
-    { key = "7"; mods = "Command"; chars = "\\u00027"; }
-    { key = "8"; mods = "Command"; chars = "\\u00028"; }
-    { key = "9"; mods = "Command"; chars = "\\u00029"; }
+    { key = "1"; mods = "Command"; chars = "${prefix}1"; }
+    { key = "2"; mods = "Command"; chars = "${prefix}2"; }
+    { key = "3"; mods = "Command"; chars = "${prefix}3"; }
+    { key = "4"; mods = "Command"; chars = "${prefix}4"; }
+    { key = "5"; mods = "Command"; chars = "${prefix}5"; }
+    { key = "6"; mods = "Command"; chars = "${prefix}6"; }
+    { key = "7"; mods = "Command"; chars = "${prefix}7"; }
+    { key = "8"; mods = "Command"; chars = "${prefix}8"; }
+    { key = "9"; mods = "Command"; chars = "${prefix}9"; }
   ];
 in
 {
