b0361d67dd5796520a24ad46797e5d2969ce28ea
Julian Antonielli <julianantonielli@gmail.com>
2026-06-11 14:41:04 +0100

    fix: migrate nvim-treesitter to new API (configs module removed)

--- modules/home/neovim/lua/user/treesitter.lua
+++ modules/home/neovim/lua/user/treesitter.lua
@@ -1,31 +1,27 @@
-require("nvim-treesitter.configs").setup({
-  highlight = {
-    enable = true,
-  },
-  playground = {
-    enable = true,
-    updatetime = 10,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["aF"] = "@block.outer",
-        ["iF"] = "@block.inner",
-      },
-    },
-    move = {
-      enable = true,
-      -- Set jumps in the jumplist
-      set_jumps = true,
-      goto_next_start = {
-        ["[f"] = "@function.outer",
-      },
-      goto_previous_start = {
-        ["]f"] = "@function.outer",
-      },
-    },
-  },
+-- Enable treesitter highlighting for all buffers that have a parser
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    if pcall(vim.treesitter.start, args.buf) then
+      -- Enable indentation via treesitter
+      vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
 })
+
+-- Textobjects (requires nvim-treesitter-textobjects plugin)
+require("nvim-treesitter-textobjects").setup({
+  move = { set_jumps = true },
+})
+
+local select_to = require("nvim-treesitter-textobjects.select").select_textobject
+local move = require("nvim-treesitter-textobjects.move")
+
+-- Select
+vim.keymap.set({ "x", "o" }, "af", function() select_to("@function.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "if", function() select_to("@function.inner", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "aF", function() select_to("@block.outer", "textobjects") end)
+vim.keymap.set({ "x", "o" }, "iF", function() select_to("@block.inner", "textobjects") end)
+
+-- Move
+vim.keymap.set({ "n", "x", "o" }, "[f", function() move.goto_next_start("@function.outer", "textobjects") end)
+vim.keymap.set({ "n", "x", "o" }, "]f", function() move.goto_previous_start("@function.outer", "textobjects") end)
