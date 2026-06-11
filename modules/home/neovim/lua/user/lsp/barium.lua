5c95f0c5d4bf8b6c91d205c7f8ea9f7d477a5807
Julian Antonielli <julianantonielli@gmail.com>
2026-06-11 14:54:08 +0100

    fix: migrate LSP config to native vim.lsp.config API (#28)
    Remove lsp-zero and nvim-lspconfig dependencies (deprecated).
    Use vim.lsp.config + vim.lsp.enable (neovim 0.11+ native API).

--- modules/home/neovim/lua/user/lsp/barium.lua
+++ modules/home/neovim/lua/user/lsp/barium.lua
@@ -1,15 +1,1 @@
-local util = require('lspconfig.util')
-
-vim.filetype.add({ filename = { Config = "brazil-config" } })
-
--- Taken from https://w.amazon.com/bin/view/Barium/#Hnvim-lspconfig.
-return {
-  default_config = {
-    cmd = {'barium'};
-    filetypes = {'brazil-config'};
-    root_dir = function(fname)
-      return util.find_git_ancestor(fname)
-    end;
-    settings = {};
-  };
-}
+-- Barium config is now in user/lsp.lua via vim.lsp.config
