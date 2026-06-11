d85d9ac81e02dccfa30e8254909ddbb37c21a5fa
Julian Antonielli <julianantonielli@gmail.com>
2026-06-11 11:20:14 +0100

    feat: add SSH config with host aliases for cloud desktops

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./fish.nix
     ./git.nix
+    ./ssh.nix
     ./starship.nix
     ./neovim
     ./tmux
@@ -11,10 +12,6 @@
   xdg.enable = true;
 
   home = {
-    sessionVariables = {
-      SSH_AUTH_SOCK = "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-    };
-
     packages = with pkgs; [
       ripgrep
       fd
